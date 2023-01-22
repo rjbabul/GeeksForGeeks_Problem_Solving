ae525ac2af61df3bd802daecfeae33738fa12d8e
Babul Akter <rjbabul420@gmail.com>
2023-01-22 22:28:14 +0600

    update

--- Kth smallest element.cpp
+++ Kth smallest element.cpp
@@ -1,3 +1,11 @@
+//{ Driver Code Starts
+//Initial function template for C++
+
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+//User function template for C++
 
 class Solution{
     public:
@@ -5,12 +13,43 @@ class Solution{
     // l : starting index of the array i.e 0
     // r : ending index of the array i.e size-1
     // k : find kth smallest element and return using this function
-    int call(int a[], int n, int k){
-
-        sort(a, a+n);
-        return a[k-1];
-    }
     int kthSmallest(int arr[], int l, int r, int k) {
-        return call(arr,r+1, k );
+        //code here
+        priority_queue< int , vector<int> , greater<int> > q;
+        for(int i=l; i<=r;i++)
+        {
+            q.push(arr[i]);
+        }
+        k--;
+        while(k--)
+        {
+            q.pop();
+        }
+
+        return q.top();
     }
 };
+
+//{ Driver Code Starts.
+
+int main()
+{
+    int test_case;
+    cin>>test_case;
+    while(test_case--)
+    {
+        int number_of_elements;
+        cin>>number_of_elements;
+        int a[number_of_elements];
+
+        for(int i=0;i<number_of_elements;i++)
+            cin>>a[i];
+
+        int k;
+        cin>>k;
+        Solution ob;
+        cout<<ob.kthSmallest(a, 0, number_of_elements-1, k)<<endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
